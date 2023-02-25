0db6074dad98d0db420b97797cadcf67cb73fa43
Jack <126353580+kackjelly@users.noreply.github.com>
2023-02-25 22:12:08 +0000

    Update base.js

--- base.js
+++ base.js
@@ -3,12 +3,4 @@ $(document).ready(function() {
     var anchorId = $(this).attr('nav-1');
     $('#' + anchorId).css('border-bottom', '2px solid #333');
   });
-  $('h1.header-title:contains("Portfolio")').each(function() {
-    var anchorId = $(this).attr('nav-2');
-    $('#' + anchorId).css('border-bottom', '2px solid #333');
-  });
-  $('h1.header-title:contains("Info")').each(function() {
-    var anchorId = $(this).attr('nav-3');
-    $('#' + anchorId).css('border-bottom', '2px solid #333');
-  });
 });

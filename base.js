9de6168b6aa0a134ecd9bdbb91404e6b8b5bb694
Jack <126353580+kackjelly@users.noreply.github.com>
2023-02-25 22:18:17 +0000

    Update base.js

--- base.js
+++ base.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  $('h1.header-title:contains("Homepage")').each(function() {
-    var anchorId = $(this).attr('nav-1');
-    $('#' + anchorId).css('border-bottom', '2px solid #333');
-  });
-});
-$().ready(function() {
-   $("#nav-1").html("Text added by jQuery code.");
-});
+
